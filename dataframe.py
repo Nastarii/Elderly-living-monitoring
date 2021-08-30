843f56f20b67564fecd29f455ad1192145bf8866
Lucas Nastari Ziza <76565870+Nastarii@users.noreply.github.com>
2021-08-29 21:36:26 -0300

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,6 +1,7 @@
-import csv
-from time import sleep
 from random import uniform
+from time import sleep
+import pathlib
+import csv
 
 class Dataframe():
 
@@ -26,38 +27,9 @@ class Dataframe():
 
     def get(self):
 
-        diret = '/home/lucas/Documentos/Testes/'
+        diret = str(pathlib.Path(__file__).parent.resolve()) +'/Dataframe_cache'
         with open(diret + 'Dataframe.csv', 'w') as output:
             writer = csv.writer(output,delimiter=";")
             
             for value in self.dic.values():
                 writer.writerow([value])
-'''     
-cont = 0
-df = Dataframe()
-
-while True:
-
-    dados = [round(uniform(0,15),2) for _ in range(11)]
-    df.put(dados,cont)
-    cont = cont + 1
-    sleep(1)
-    if cont == 5:
-        break
-
-
-a = [i for i in range(11)]
-b = [j for j in range(18)]
-df.insert2(a)
-df.insert2(b)
-
-
-df.get()
-
-def insert(self,value):
-        position = min(self.dic.keys())
-        self.dic[int(position) + self.cont].append(value)
-        self.cont = self.cont + 1
-        if self.cont == len(self.dic):
-            self.cont = 0
-        return self.dic'''
